5a83adcbf90bc3396b3caa90f9e52f94627238db
Daniel <danielgola@vastudio.pl>
2023-03-09 16:23:38 +0100

    Mobile nav finished

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const navMain = document.querySelector('.nav__main-nav');
-const navOthers = document.querySelector('.nav__others-nav');
 const navBtn = document.querySelector('.nav__burgerBtn');
 
 const allNavItems = document.querySelectorAll('.nav__item');
@@ -8,12 +7,10 @@ const allNavItems = document.querySelectorAll('.nav__item');
 
 const handleNav = () => {
     navMain.classList.toggle('nav--active');
-    navOthers.classList.toggle('nav--active');
 
     allNavItems.forEach(item => {
         item.addEventListener('click', () => {
             navMain.classList.remove('nav--active');
-            navOthers.classList.remove('nav--active');
         })
     })
 }
