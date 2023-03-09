a7912201367dd9cb254fcd2367bdd2ca0affb7fa
Daniel <danielgola@vastudio.pl>
2023-03-09 21:21:44 +0100

    Navigaion complited

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const navMain = document.querySelector('.nav__main-nav');
+const navSocial = document.querySelector('.nav__social');
 const navBtn = document.querySelector('.nav__burgerBtn');
 const navShadow = document.querySelector('.nav__shadow');
 
@@ -8,11 +9,13 @@ const allNavItems = document.querySelectorAll('.nav__item');
 
 const handleNav = () => {
     navMain.classList.toggle('nav--active');
+    navSocial.classList.toggle('nav--active');
     navShadow.classList.toggle('nav__shadow--active');
 
     allNavItems.forEach(item => {
         item.addEventListener('click', () => {
             navMain.classList.remove('nav--active');
+            navSocial.classList.remove('nav--active');
             navShadow.classList.remove('nav__shadow--active');
         })
     })
