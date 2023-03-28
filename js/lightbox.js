5d05ab7f8379432e4fe45e24cd45d159d79b0db5
Daniel <danielgola@vastudio.pl>
2023-03-28 19:54:00 +0200

    home page - to be reviewed

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,3 +1,4 @@
 lightGallery(document.querySelector('.galleryOne'));
 lightGallery(document.querySelector('.galleryTwo'));
 lightGallery(document.querySelector('.galleryThree'));
+lightGallery(document.querySelector('.galleryFour'));
