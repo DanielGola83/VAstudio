0d40a6c103c3e3ffbbbae4c3718d5e9b94136bf6
Daniel <danielgola@vastudio.pl>
2023-04-02 14:09:26 +0200

    02.04.2023

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
+
 const navMain = document.querySelector('.nav__main-nav');
-const navSocial = document.querySelector('.nav__social');
 const navBtn = document.querySelector('.nav__burgerBtn');
 const navShadow = document.querySelector('.nav__shadow');
+const footerYear = document.querySelector('.footer__year')
 
 const allNavItems = document.querySelectorAll('.nav__item');
 
 
-
 const handleNav = () => {
     navMain.classList.toggle('nav--active');
-    navSocial.classList.toggle('nav--active');
     navBtn.classList.toggle('open--active');
     navShadow.classList.toggle('nav__shadow--active');
 
     allNavItems.forEach(item => {
         item.addEventListener('click', () => {
             navMain.classList.remove('nav--active');
-            navSocial.classList.remove('nav--active');
             navBtn.classList.remove('open--active');
             navShadow.classList.remove('nav__shadow--active');
         })
@@ -25,3 +23,22 @@ const handleNav = () => {
 
 navBtn.addEventListener('click', handleNav);
 navShadow.addEventListener('click', handleNav)
+
+
+// FOOTER YEAR - START
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year
+}
+handleCurrentYear();
+
+
+// DROPPING CONTACT
+const navDropContact = document.querySelector('.nav__droppingContact')
+
+const droppingContactShow = () => {
+    navDropContact.classList.toggle('nav__droppingContact--show', window.scrollY > 700);
+    navDropContact.classList.toggle('nav__droppingContact--hide', window.scrollY > 11500);
+}
+
+window.addEventListener('scroll', droppingContactShow)
