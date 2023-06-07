a7ec1829c2dd444b21ea644a9dfa75a492d30e6e
Daniel <danielgola@vastudio.pl>
2023-06-07 15:56:36 +0200

    CompletionOfWorkOnTheWebsite

--- js/main.js
+++ js/main.js
@@ -61,3 +61,27 @@ const popUpWindowOff = () => {
 CTA.addEventListener('click', popUpWindowOn)
 CTAshadow.addEventListener('click', popUpWindowOff)
 CTAcloseBtn.addEventListener('click', popUpWindowOff)
+
+
+// ACCORDION
+const accordionItemHeader = document.querySelectorAll(".section-accordion__header")
+
+accordionItemHeader.forEach(accordionItemHeader => {
+    accordionItemHeader.addEventListener("click", event => {
+
+        // const currentlyActiveAccordionItemHeader = document.querySelector(".section-accordion__header.active");
+        // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+        //     currentlyActiveAccordionItemHeader.classList.toggle("active");
+        //     currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0
+        // }
+
+        accordionItemHeader.classList.toggle("active");
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+        if(accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        }
+        else {
+            accordionItemBody.style.maxHeight = 0
+        }
+    })
+})
